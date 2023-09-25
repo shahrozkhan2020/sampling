6abfb4ceab883d51dbecce3bd21f37eb105a7b06
shahrozkhan2020 <138502301+shahrozkhan2020@users.noreply.github.com>
2023-09-25 12:00:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from SJAYA import SJAYA
 import numpy as np
 import boto3
+import os
 
-paraRange = [[0, 0],[1 ,1]]
+# Get the current working directory
+base_path = os.getcwd() + "/"
+
+# Bucket name
+bucket_name = input("Enter the bucket name: ")
+
+paraRange = [[0, 0], [1, 1]]
 numOfPara = len(paraRange[0])
 numOfDesigns = 10
 popSize = 10
@@ -12,25 +19,15 @@ for i in range(5):
     s.PerformSJAYA(numOfPara, paraRange, save_path=f"output_plot_{i}.png")
     print(f"Sampling {i} done!")
 
-base_path = "/home/ubuntu/test/sampling/"
-
-# Initialize the S3 client outside the loop, so you don't have to do it multiple times
+# Initialize the S3 client outside the loop
 s3 = boto3.client('s3')
 
 # Loop over the range of file indices
-for i in range(5):  # This will iterate over numbers 1 through 5
+for i in range(5):  # This will iterate over numbers 0 through 4
     file_name = f"output_plot_{i}.png"
     file_path = base_path + file_name
 
     with open(file_path, "rb") as f:
-        s3.upload_fileobj(f, "samplingtestbucket", file_name)
+        s3.upload_fileobj(f, bucket_name, file_name)
     print(f"File {file_name} added to bucket!")
 
-"""
-# create the 2D array
-arr = np.zeros((rows, columns))
-for i in range(columns):
-    arr[:, i] = np.random.uniform(paraRange[0,i], paraRange[1,i], size=rows)
-
-print(arr)
-"""
